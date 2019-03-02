6ee872b56fca2e577e205628153dbe7a390b8735
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-02 17:50:55 -0500

    created the router on the app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/:Movie" component={MovieDetail} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
