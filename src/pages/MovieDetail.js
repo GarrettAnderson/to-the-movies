edb4a7d245c5981242f17658db621125c5f46865
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-03 17:04:13 -0500

    got the cast to render on the movie details page

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,34 +1,48 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react'
+import axios from 'axios'
 
 class MovieDetail extends Component {
   state = {
     movies: [],
-    movieId: "",
-    title: "",
-    releaseDate: "",
-    cast: "",
-    description: "",
-    image: ""
-  };
+    movieId: '',
+    title: '',
+    releaseDate: '',
+    movieCast: [],
+    description: '',
+    image: ''
+  }
 
   componentDidMount() {
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/${
-          this.props.match.params.movie
-        }?api_key=76fa766989bc43019f44915308d9d724`
+        `https://api.themoviedb.org/3/movie/${this.props.match.params.movie}?api_key=76fa766989bc43019f44915308d9d724`
       )
-      .then(resp => {
-        console.log(resp);
+      .then((resp) => {
+        console.log(resp)
 
         this.setState({
           title: resp.data.title,
           image: resp.data.poster_path,
           description: resp.data.overview,
           releaseDate: resp.data.release_date
-        });
-      });
+        })
+      })
+    this.getCast()
+  }
+
+  getCast = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${this.props.match.params
+          .movie}/credits?api_key=76fa766989bc43019f44915308d9d724`
+      )
+      .then((resp) => {
+        console.log(resp)
+
+        this.setState({
+          movieCast: resp.data.cast
+        })
+      })
   }
 
   render() {
@@ -42,11 +56,16 @@ class MovieDetail extends Component {
             <h2>{this.state.title}</h2>
             <p>{this.state.releaseDate}</p>
             <p>{this.state.description}</p>
+            <ol>
+              {this.state.movieCast.map((cast, index) => {
+                return <li key={index}>{cast.name}</li>
+              })}
+            </ol>
           </figcaption>
         </figure>
       </section>
-    );
+    )
   }
 }
 
-export default MovieDetail;
+export default MovieDetail
